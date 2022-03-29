a26faf91adcd2c6ccb7765ecec6c330d7984b7ef
Vlad Dragomir <vdragomir@protonmail.com>
2022-03-29 11:03:53 +0200

    testing a new push

---
+++ src/Treehouse.ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Treehouse.ComicBookGallery.Controllers
+{
+	public class ComicBooksController : Controller
+	{
+		public string Detail()
+		{
+			return "Hello from the ComicBooks Controller!";
+		}
+	}
+}
