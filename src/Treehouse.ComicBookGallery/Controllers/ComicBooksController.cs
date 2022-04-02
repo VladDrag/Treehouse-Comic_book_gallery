f69c570da190a0d06c4f26f3e090425963a90ea3
Vlad Dragomir <vdragomir@protonmail.com>
2022-04-02 11:20:59 +0200

    Updated Comic Book Detailed view to be strongly typed

--- src/Treehouse.ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/Treehouse.ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Treehouse.ComicBookGallery.Models;
 
 namespace Treehouse.ComicBookGallery.Controllers
 {
@@ -6,19 +7,21 @@ namespace Treehouse.ComicBookGallery.Controllers
 	{
 		public ActionResult Detail()
 		{
-
-            ViewBag.SeriesTitle = "The Amazing Spiderman";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
-                        {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-                        };
-            return View();
+			var comicBook = new ComicBook()
+			{
+				SeriesTitle = "The Amazing Spiderman",
+				IssueNumber = 700,
+				DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+				Artists = new Artist[]
+				{
+					new Artist { Name = "Dan Slott", Role = "Script"},
+					new Artist { Name = "Humberto Ramos", Role = "Pencils"},
+					new Artist { Name = " Victor Olazaba", Role = "Inks"},
+					new Artist { Name = "Edgar Delgado", Role = "Colors"},
+					new Artist { Name = "Chris Eliopoulos", Role = "Letters"}
+				},
+			};
+			return View(comicBook);
 		}
 	}
 }
