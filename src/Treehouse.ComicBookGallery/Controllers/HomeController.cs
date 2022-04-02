9a742aec52f13cdd6ae717721825430161476680
Vlad Dragomir <vdragomir@protonmail.com>
2022-04-02 16:08:46 +0200

    Updated Comic Books List View

--- src/Treehouse.ComicBookGallery/Controllers/HomeController.cs
+++ src/Treehouse.ComicBookGallery/Controllers/HomeController.cs
@@ -1,21 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Treehouse.ComicBookGallery.Data;
 using Treehouse.ComicBookGallery.Models;
 
 namespace Treehouse.ComicBookGallery.Controllers
 {
 	public class HomeController : Controller
 	{
+		private ComicBookRepository _comicBookRepository = null;
+		public HomeController()
+		{
+			_comicBookRepository = new ComicBookRepository();
+		}
 		private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+		//public HomeController(ILogger<HomeController> logger)
+		//{
+		//	_logger = logger;
+		//}
 
-		public IActionResult Index()
+		public ActionResult Index()
 		{
-			return View();
+			var comicBooks = _comicBookRepository.GetComicBooks();
+			return View(comicBooks);
 		}
 
 		public IActionResult Privacy()
